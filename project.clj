4565b19be3fdfc155f132479d92e249095dfe18c
Zack <zack@zacharymaril.com>
2014-01-02 20:58:21 +0000

    init

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject callcongressnow "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://callcongressnow.herokuapp.com"
+  :license {:name "FIXME: choose"
+            :url "http://example.com/FIXME"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [compojure "1.1.1"]
+                 [ring/ring-jetty-adapter "1.1.0"]
+                 [ring/ring-devel "1.1.0"]
+                 [ring-basic-authentication "1.0.1"]
+                 [environ "0.2.1"]
+                 [com.cemerick/drawbridge "0.0.6"]]
+  :min-lein-version "2.0.0"
+  :plugins [[environ/environ.lein "0.2.1"]]
+  :hooks [environ.leiningen.hooks]
+  :profiles {:production {:env {:production true}}})
---
+++ test/callcongressnow/web_test.clj
@@ -0,0 +1,6 @@
+(ns callcongressnow.web-test
+  (:require [clojure.test :refer :all]
+            [callcongressnow.web :refer :all]))
+
+(deftest first-test
+  (is false "Tests should be written"))
